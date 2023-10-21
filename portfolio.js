fd973e9672766fffab679377e54699cdd885aa5e
Aditya Dwivedi <146622862+AdityaDwivedi1611@users.noreply.github.com>
2023-10-22 03:51:08 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,16 @@
+var typed=new Typed(".text", {
+    strings:[" Coding Enthusiast"," Web Developer"],
+    typeSpeed:80,
+    backSpeed:80,
+    backDelay:1000,
+    loop:true
+});
+document.getElementById("linkedin").onclick=function(){
+    window.location.href="https://www.linkedin.com/in/aditya-dwivedi-497090227?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+}
+document.getElementById("github").onclick=function(){
+    window.location.href="https://github.com/AdityaDwivedi1611"
+}
+document.getElementById("codingninja").onclick=function(){
+    window.location.href="https://www.codingninjas.com/studio/profile/ae3452f7-71a7-4107-a8eb-a55f41d58af2"
+}
